48732e0ca00fa0426842b607cb60e4386488b620
Paul Ragot <ragot.paul12@gmail.com>
2025-03-19 14:37:45 +0100

    chore: format all py files with ruff
    Run `ruff format ./*.py` to format all python files in the project.

--- yazyk/apps.py
+++ yazyk/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class YazykConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'yazyk'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "yazyk"
